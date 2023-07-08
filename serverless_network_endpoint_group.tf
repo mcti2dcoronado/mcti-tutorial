586c810a1f29c887b673e9331e2f41b3d46b847e
Douglass Coronado <dcoronado@funio.com>
2023-07-08 04:19:23 -0400

    modify endpoint

--- serverless_network_endpoint_group.tf
+++ serverless_network_endpoint_group.tf
@@ -7,8 +7,8 @@ resource "google_compute_region_network_endpoint_group" "function_neg" {
     neg-function-north-central-1  =   "us-central1"
   }
   
-  name                            =   "each.key"
-  region                          =   "each.value"
+  name                            =   each.key
+  region                          =   each.value
   network_endpoint_type           =   "SERVERLESS"
   cloud_function {
     function = google_cloudfunctions_function.my_function_neg.name
