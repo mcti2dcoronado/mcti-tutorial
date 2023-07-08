704352f342a96b9c37b778b5c4ad30de4baeb9ab
Douglass Coronado <dcoronado@funio.com>
2023-07-08 03:53:15 -0400

    modify network endpoint

--- serverless_network_endpoint_group.tf
+++ serverless_network_endpoint_group.tf
@@ -1,19 +1,41 @@
-resource "google_compute_region_network_endpoint_group" "negfetchdata1" {
-  name                  = "negfetchdata1"
-  network_endpoint_type = "SERVERLESS"
-  region                = "us-central1"
-  project = var.project_id
-  cloud_function {
-    function = "fetchData"
-  }
-}
-resource "google_compute_region_network_endpoint_group" "negfetchdata2" {
-  name                  = "negfetchdata2"
-  network_endpoint_type = "SERVERLESS"
-  region                = "northamerica-northeast1"
-  project = var.project_id
-  cloud_function {
-    function = "fetchData"
+resource "google_compute_region_network_endpoint_group" "function_neg" {
+
+  for_each = {
+    neg-function-north-east       =   "northamerica-northeast1"
+    neg-function-north-central    =   "us-central1"
+    neg-function-north-east-1     =   "northamerica-northeast1"
+    neg-function-north-central-1  =   "us-central1"
   }
   
+  name                            =   "each.key"
+  region                          =   "each.value"
+  network_endpoint_type           =   "SERVERLESS"
+  cloud_function {
+    function = google_cloudfunctions_function.my_function_neg.name
+  }
+
+}
+
+resource "google_cloudfunctions_function" "my_function_neg" {
+
+  name  = "my-function-neg"
+  runtime = "nodejs10"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "helloGet"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "my-function-net-bucket"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   =   "code-zip"
+  bucket =   google_storage_bucket.bucket.name
+  source =   "./code.zip"
 }
