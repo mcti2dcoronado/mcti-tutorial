f3cde8fba4d9e709ee16144db96e3b0e18861af4
Douglass Coronado <dcoronado@funio.com>
2023-07-08 04:06:14 -0400

    Remove duplicated variable declaration

--- var.tf
+++ var.tf
@@ -4,11 +4,6 @@ variable "project_id" {
   type = string
 }
 
-variable "backend_name" {
-  default   = "backendfetchdata"
-  type      = string
-}
-
 variable "load_balancing_scheme" {
   description = "Load balancing scheme type (EXTERNAL for classic external load balancer, EXTERNAL_MANAGED for Envoy-based load balancer, and INTERNAL_SELF_MANAGED for traffic director)"
   type        = string
