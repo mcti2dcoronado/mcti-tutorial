7344e9849853a98f53711b1cb3d172f1e9e01b9a
Douglass Coronado <dcoronado@funio.com>
2023-07-08 04:05:12 -0400

    Remove duplicated resource configurations

--- serverless_network_endpoint_group.tf
+++ serverless_network_endpoint_group.tf
@@ -15,27 +15,3 @@ resource "google_compute_region_network_endpoint_group" "function_neg" {
   }
 
 }
-
-resource "google_cloudfunctions_function" "my_function_neg" {
-
-  name  = "my-function-neg"
-  runtime = "nodejs10"
-
-  available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.archive.name
-  trigger_http          = true
-  timeout               = 60
-  entry_point           = "helloGet"
-}
-
-resource "google_storage_bucket" "bucket" {
-  name     = "my-function-net-bucket"
-  location = "US"
-}
-
-resource "google_storage_bucket_object" "archive" {
-  name   =   "code-zip"
-  bucket =   google_storage_bucket.bucket.name
-  source =   "./code.zip"
-}
