bb8eb659e77df96c8b58111c1334309539a1a45e
Douglass Coronado <dcoronado@funio.com>
2023-07-08 03:54:30 -0400

    Modify url map

--- urlmap.tf
+++ urlmap.tf
@@ -1,19 +1,20 @@
-resource "google_compute_url_map" "serverlesshttploadbalancer" {
-  default_service = google_compute_backend_service.defaultbackend.self_link
-  name = "serverlesshttploadbalancer"
-  project = var.project_id
+resource "google_compute_url_map" "my_url_map_serverless" {
+  for_each          =   google_compute_backend_service.my_backend_service
+  default_service   =   each.name.self_link
+  name              =   "my_url_map_serverless"
+  project           =    var.project_id
   host_rule {
     hosts        = ["montreal_college"]
     path_matcher = "path-matcher"
   }
 
   path_matcher {
-    default_service = google_compute_backend_service.defaultbackend.self_link
+    default_service = google_compute_backend_service.my_backend_service.my-backend-1-northamerica-northeast1.id
     name            = "path-matcher"
 
         path_rule {
         paths   = ["/fetchdata"]
-        service = google_compute_backend_service.backendfetchdata.self_link
+        service = google_compute_backend_service.my_backend_service.my-backend-2-us-central1.id
         }
   }
 }
